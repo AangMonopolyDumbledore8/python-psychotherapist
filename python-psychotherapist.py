500d577a4299ad25c05c493eeff3ac417748e23e
albusdumbledore <spamwhenever@gmail.com>
2017-10-28 12:08:32 -0400

    initial commit

---
+++ python-psychotherapist.py
@@ -0,0 +1,27 @@
+#created by Andrew Ghastine
+#with help from Baylen
+
+#imports
+import sys
+import time
+import random
+#Colour codes
+w  = "\033[0m" # white (normal)
+r  = "\033[31m" # red
+g  = "\033[32m" # green
+o  = "\033[33m" # orange
+b  = "\033[34m" # blue
+p  = "\033[35m" # purple
+#variables
+problem = "someone hit you"
+#lists
+questions = ["what's your problem?", "why do you feel that way?", ("Is it because" + problem + "that you feel that way?")]
+#userinput
+userinput = input(b+random.choice(questions)+": "+w)
+
+x = 1
+while True:
+    if (random.choice(questions)) == "what's your problem?":
+        problem = userinput
+    userinput = input(random.choice(questions)+": ")
+print ("done")
