74c8f0bcf9ef23c9b7a580f217839a69e4a84af1
AangMonopolyDumbledore8 <31523431+AangMonopolyDumbledore8@users.noreply.github.com>
2017-10-28 17:10:34 -0400

    no longer uses 1st Prob evry time, evry qston's answr is problem
    'Name something bad that happened in your life: war
    why do you feel that way?: i don't konw
    what's your problem?: hunger
    why do you feel that way?: ???
    Is it because of ??? that you feel that way?: no
    why do you feel that way?: I'm pretty sure punctuation isn't behind it
    why do you feel that way?: logic
    what's your problem?: famine
    Is it because of famine that you feel that way?: probably
    what's your problem?: i told you already, famine
    what's your problem?: What is wrong with you
    what's your problem?: you are the worst
    what's your problem?: go away
    why do you feel that way?: you are being very annoying
    Is it because of you are being very annoying that you feel that way?: '
    see how on the second to last line, the answer was the problem in the next question even though the question wasn't "what's your problem.

--- psychotherapist.py
+++ psychotherapist.py
@@ -5,7 +5,7 @@
 import sys
 import time
 import random
-#Colour codes
+#Color codes
 w  = "\033[0m" # white (normal)
 r  = "\033[31m" # red
 g  = "\033[32m" # green
@@ -13,18 +13,22 @@ o  = "\033[33m" # orange
 b  = "\033[34m" # blue
 p  = "\033[35m" # purple
 #variables
-problem = "someone hit you"
+problemDefault = input(p+ "Name something bad that happened in your life: ")
+counter = 0
+#Definitions
+#addToVar()
+#nonlocal counter = counter + 1
 #lists
-questions = ["what's your problem?", "why do you feel that way?", ("Is it because" + problem + "that you feel that way?")]
+questions = ["what's your problem?", "why do you feel that way?", ("Is it because of " + problemDefault + " that you feel that way?")]
 #userinput
 userinput = input(b+random.choice(questions)+": "+w)
-amountOftimesQuestionOnewasAsked = 0
 x = 1
 if (random.choice(questions)) == "what's your problem?":
-    amountOftimesQuestionOnewasAsked = amountOftimesQuestionOnewasAsked + 1
+    addToVar()
 while True:
     if (random.choice(questions)) == "what's your problem?":
-        if (amountOftimesQuestionOnewasAsked) <= 1:
-            problem = userinput
+        #if (counter) <= 1:
             userinput = input(random.choice(questions)+": ")
+            problem = userinput
+            questions = ["what's your problem?", "why do you feel that way?", ("Is it because of " + problem + " that you feel that way?")]
 print ("done")
